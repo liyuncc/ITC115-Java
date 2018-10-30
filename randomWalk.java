cba889a6efb852471d19338d8c791fb7195edd37
Liyun Cecil <liyuncecil@gmail.com>
2018-10-29 18:58:12 -0700

    Update randomWalk.java

--- randomWalk.java
+++ randomWalk.java
@@ -1,9 +1,38 @@
+import java.util.Random;
 
-public class randomWalk {
+public class Midterm {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Random r = new Random();
+	randomWalk();
+	
 
+
+private static void randomWalk() {
+	// TODO Auto-generated method stub
+	int num = 0;
+	int max= 3;
+	int min =-3;
 }
+	while (!(max==min)) {
+		int x = r.nextInt();
+		num++;
+		System.out.println("position = " + x);
+		
+	}
+	
+}
+
+
+	
+	
+
+	
+	
+}
+
+	
+
+
