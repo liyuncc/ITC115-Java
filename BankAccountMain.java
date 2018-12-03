d5acac41429caf960d4b9995d0a31a842d7952c6
Liyun Cecil <liyuncecil@gmail.com>
2018-12-02 16:25:21 -0800

    Assignment 8

---
+++ BankAccountMain.java
@@ -0,0 +1,21 @@
+
+public class BankAccountMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankAccount Ian = new BankAccount("Ian", 100.00, 2.50);
+				
+		Ian.deposit(50.00);
+		System.out.println(Ian.balance);
+		System.out.println(Ian.toString());
+		
+		Ian.withdraw(200.00);
+		System.out.println(Ian.balance);
+		System.out.println(Ian.toString());
+		
+		Ian.withdraw(75.00);
+		System.out.println(Ian.balance);
+		System.out.println(Ian.toString());
+	}
+
+}
